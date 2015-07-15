d802e24febe9e5d2344629de24d0df2a76e5eb70
Allan lukwago <epicallan.al@gmail.com>
2015-07-15 19:20:22 +0300

    reading files and lists

--- week1/reading_files.py
+++ week1/reading_files.py
@@ -24,17 +24,42 @@
 
 #print os.path.dirname("/week1/def.py")
 
-file=open("C:/Users/USER/devinitpy.github.io/week1/oracle.txt","r")
+#A function that opens a file and returns data with in the file
 
-data=file.readlines()
-#print(data)
-n =0
-for i in data:
-    n=n+1
-    list=i.split(" ")
-    if n == len(data) -1:
-        #we are on the last value
-        print("last for loop iteration")
-        #format it differently
-        print(list)
-file.close()
+file_location = "C:/Users/USER/devinitpy.github.io/week1/text.txt"
+file_location1= "C:/Users/USER/devinitpy.github.io/week1/text2.txt"
+file_location2 = "C:/Users/USER/devinitpy.github.io/week1/text3.txt"
+
+files=[file_location,file_location1,file_location2]
+
+def open_file(file_name):
+    file=open(file_name,"r")
+    data=file.readlines()
+    return data
+
+i =0
+for i in range(0,3):
+    current_file=files[i]
+    d=open_file(current_file)
+    i=i+1
+    print(d)
+
+# for item in data:
+#     content =item.split(" ")
+#     #print(content)
+#     name=content[0]
+#     marks=content[1]
+#     print("name: "+name)
+#     print("marks: "+marks)
+
+
+# n =0
+# for i in data:
+#     n=n+1
+#     list=i.split(" ")
+#     if n == len(data) -1:
+#         #we are on the last value
+#         print("last for loop iteration")
+#         #format it differently
+#         print(list)
+# file.close()
